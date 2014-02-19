45afd7adf5388a16e035c7264f42b918c804e93b
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2014-02-19 07:43:14 +0100

    Include the dummy environment when it exists.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ SimpleCov.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../dummy/config/environment", __FILE__)
+require File.expand_path("../dummy/config/environment", __FILE__) if Dir.exists?("spec/dummy")
 require 'rspec/rails'
 require 'rspec/autorun'
 require "capybara/rails"
