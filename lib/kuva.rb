37df2358826e7a28efdbfd7d0728bf09539d4c23
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2014-02-22 19:50:38 +0100

    Added the ability to configure Kuva through a setup block, which can be specified in the config/initializers directory of a Rails application.

--- lib/kuva.rb
+++ lib/kuva.rb
@@ -1,6 +1,11 @@
 require "flickraw"
 
 module Kuva
+  mattr_accessor :api_key, :shared_secret, :access_token, :access_token_secret
+
+  def self.setup
+    yield self
+  end
 end
 
 require "kuva/engine"
