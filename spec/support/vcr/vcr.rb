338afbbf9e5e570d5911443e73430eb515413bd1
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2014-04-13 01:47:13 +0200

    Let's allow HTTP connections when there is no VCR cassette for it.

--- spec/support/vcr/vcr.rb
+++ spec/support/vcr/vcr.rb
@@ -1,6 +1,7 @@
 require "vcr"
 
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/support/vcr/cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
