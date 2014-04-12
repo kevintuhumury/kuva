a024462541d72175c99a84bd9684f3dc8e035794
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2014-04-13 01:44:04 +0200

    Since we've set the Kuva settings in the spec helper, let's just stub the method we're testing in our it blocks and keep the other settings at its original values.

--- spec/controllers/kuva/application_controller_spec.rb
+++ spec/controllers/kuva/application_controller_spec.rb
@@ -8,29 +8,32 @@ module Kuva
       end
     end
 
-    before do
-      allow(Kuva).to receive(:api_key).and_return "<api_key>"
-      allow(Kuva).to receive(:shared_secret).and_return "<shared_secret>"
-      allow(Kuva).to receive(:access_token).and_return "<access_token>"
-      allow(Kuva).to receive(:access_secret).and_return "<access_secret>"
-    end
-
     context "configuring Kuva", :vcr do
-      before { get :index }
-
       it "sets the API key" do
+        allow(Kuva).to receive(:api_key).and_return "<api_key>"
+        get :index
+
         expect(FlickRaw.api_key).to eq "<api_key>"
       end
 
       it "sets the shared secret" do
+        allow(Kuva).to receive(:shared_secret).and_return "<shared_secret>"
+        get :index
+
         expect(FlickRaw.shared_secret).to eq "<shared_secret>"
       end
 
       it "sets the access token" do
+        allow(Kuva).to receive(:access_token).and_return "<access_token>"
+        get :index
+
         expect(flickr.access_token).to eq "<access_token>"
       end
 
       it "sets the access secret" do
+        allow(Kuva).to receive(:access_secret).and_return "<access_secret>"
+        get :index
+
         expect(flickr.access_secret).to eq "<access_secret>"
       end
     end
