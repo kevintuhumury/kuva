c521ea5f6fdb449a616a953bd2ad45874e83e647
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2014-04-13 19:39:55 +0200

    Let's use the environment variables (from .env) to fill in the Kuva setup block in the dummy application, so we can actually walk through the application in the development environment.

--- spec/dummy/config/initializers/kuva.rb
+++ spec/dummy/config/initializers/kuva.rb
@@ -8,22 +8,22 @@ Kuva.setup do |config|
   # The Flickr API requires an API key to connect your application
   # to your Flickr account. You can apply for an API key here:
   # http://www.flickr.com/services/apps/create/apply.
-  config.api_key = "API_KEY"
+  config.api_key = ENV["KUVA_API_KEY"]
 
   # Besides the API key, the Flickr API requires a shared secret.
   # Both the API key and shared secret are used to sign requests
   # to the Flickr API and to retrieve an Access Token and Access
   # Secret.
-  config.shared_secret = "SHARED_SECRET"
+  config.shared_secret = ENV["KUVA_SHARED_SECRET"]
 
   # As specified above, the Access Token can be retrieved by using
   # the kuva:authorize Rake task. Once you've done that, enable the
   # configuration option below and set it's value.
-  # config.access_token = "ACCESS_TOKEN"
+  config.access_token = ENV["KUVA_ACCESS_TOKEN"]
 
   # As specified above, both the Access Token as well as the Access
   # Secret can be retrieved by using the kuva:authorize Rake task.
   # Once you've done that, enable this configuration option and set
   # it's value.
-  # config.access_secret = "ACCESS_SECRET"
+  config.access_secret = ENV["KUVA_ACCESS_SECRET"]
 end
